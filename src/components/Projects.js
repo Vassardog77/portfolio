245d176eb00494565675aa7e70908052fd4f54d5
Vassardog77 <benmoxon256@gmail.com>
2023-05-04 16:34:51 -0700

    first project description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ function Projects(props) {
             <div className='heading'>Projects</div>
             <div className='project_element_right'>
                 <img src={`${process.env.PUBLIC_URL}/images/forest.jpg`} alt="pygame project"></img>
-                <div className='project_element_text'>This is a pygame maze project that I created for blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</div>
+                <div className='project_element_text'>A pygame raycasting maze project. This taught me a lot about the intrcacies of python, game development, and applied trigonometry.</div>
             </div>
             <div className='project_element_left'>
                 <img src={`${process.env.PUBLIC_URL}/images/forest.jpg`} alt="pygame project"></img>
