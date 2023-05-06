08d8d2536f263aee59acce75c9eec3d604456a89
Vassardog77 <benmoxon256@gmail.com>
2023-05-06 16:43:04 -0700

    first finished version, improved on projects+res

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,21 +4,33 @@ function Projects(props) {
     return (
         <div id='projects' className='section_parent'>
             <div className='heading'>Projects</div>
-            <div className='project_element_right'>
-                <img src={`${process.env.PUBLIC_URL}/images/forest.jpg`} alt="pygame project"></img>
-                <div className='project_element_text'>A pygame raycasting maze project. This taught me a lot about the intrcacies of python, game development, and applied trigonometry.</div>
+            <div className='project_parent'>
+                <div className='project_element1'>
+                <div className='project_heading'>Bitcoin Mining Optimizations</div>
+                    <img src={`${process.env.PUBLIC_URL}/images/bitcoin_project.png`} alt="bitcoin project"></img>
+                    <div className='project_element_text'>My final project for my undergraduate degree. An optimization of the double SHA256 bitcoin mining algorithm, coded in system verilog.</div>
+                    <a href='https://github.com/Vassardog77/SHA256_optimizations' target='_blank' rel='noreferrer'>Check out the code!</a>
+                </div>
+                <div className='project_element2'>
+                <div className='project_heading'>Social Media Manager</div>
+                    <img src={`${process.env.PUBLIC_URL}/images/media_project.png`} alt="media project"></img>
+                    <div className='project_element_text'>By far the a largest project I've ever done. A fully functional MERN stack social media manager. Unfortuanly due to api app restrictions, the application will only work for test users.</div>
+                    <a href='https://github.com/Vassardog77/Network4_test' target='_blank' rel='noreferrer'>Check out the code!</a>
+                </div>
             </div>
-            <div className='project_element_left'>
-                <img src={`${process.env.PUBLIC_URL}/images/forest.jpg`} alt="pygame project"></img>
-                <div className='project_element_text'>This is a pygame maze project that I created for blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</div>
-            </div>
-            <div className='project_element_right'>
-                <img src={`${process.env.PUBLIC_URL}/images/forest.jpg`} alt="pygame project"></img>
-                <div className='project_element_text'>This is a pygame maze project that I created for blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</div>
-            </div>
-            <div className='project_element_left'>
-                <img src={`${process.env.PUBLIC_URL}/images/forest.jpg`} alt="pygame project"></img>
-                <div className='project_element_text'>This is a pygame maze project that I created for blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</div>
+            <div className='project_parent2'>
+                <div className='project_element4'>
+                    <div className='project_heading'>Pygame Raycasing</div>
+                    <img src={`${process.env.PUBLIC_URL}/images/pygame_project.png`} alt="pygame project"></img>
+                    <div className='project_element_text'>A pygame raycasting maze game. This taught me a lot about the intrcacies of python, game development, and applied trigonometry.</div>
+                    <a className='link' href='https://github.com/Vassardog77/raycasting_maze' target='_blank' rel='noreferrer'>Check out the code!</a>
+                </div>
+                <div className='project_element3'>
+                <div className='project_heading'>Portfolio Website</div>
+                    <img src={`${process.env.PUBLIC_URL}/images/portfolio_project.png`} alt="portfolio project"></img>
+                    <div className='project_element_text'>The porfolio website you're looking at right now was coded completely from scratch using React. It's a great showcase of my frontend development skills.</div>
+                    <a href='https://github.com/Vassardog77/portfolio' target='_blank' rel='noreferrer'>Check out the code!</a>
+                </div>
             </div>
         </div>
     );
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,6 +4,32 @@ function Resume(props) {
     return (
         <div id='resume' className='section_parent'>
             <div className='heading'>Resume</div>
+            <div className='resume_parent'>
+                <div className='resume_list'>
+                    <div className='sub_heading'>Skills</div>
+                    <ul>
+                        <li>Full Stack Web Development (MERN Stack)</li>
+                        <li>React</li>
+                        <li>Python</li>
+                        <li>C</li>
+                        <li>Java</li>
+                        <li>Javascript</li>
+                        <li>HTML</li>
+                        <li>SQL</li>
+                        <li>Verilog and VHDLs</li>
+                        <li>Computer Aided Design (Onshape, SolidWorks)</li>
+                        <li>Circuit Design (Quartus, and physical design)</li>
+                    </ul>
+                    </div>
+                <div className='resume_list'>
+                    <div className='sub_heading'>Experience</div>
+                    <ul>
+                        <li>No Boundaries Marketing Group (5/22 – current) (writing webpages for clients)</li>
+                        <li>Chandler Innovations Internship (5/21-8/21) (web page design)</li>
+                    </ul>
+                    <a href={`${process.env.PUBLIC_URL}/images/Resume_4.14.23.pdf`} download>Download Resume PDF</a>
+                </div>
+            </div>
         </div>
     );
 }
